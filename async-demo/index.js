ced589a6090a7b282f71677c5cb3d0c1e1beec08
@zolabera <zolyemek@gmail.com>
2024-01-15 16:52:08 +0300

    consuming promises

--- async-demo/index.js
+++ async-demo/index.js
@@ -1,42 +1,39 @@
 console.log("Before");
-getUser(1, getUserRepos);
+
+getUser(1)
+  .then((user) => getRepos(user.gitHubUserName))
+  .then((repos) => getCommits(repos[0]))
+  .then((commits) => console.log("commits", commits))
+  .catch(err=> console.log('Error', err.message));
+
 console.log("After");
 
-function getUserRepos(user){
-    console.log("User Repos", user);
-    getRepos(user.gitHubUserName, getUserCommits)
-}
-function getUserCommits(repos){
-    console.log("User Repos", repos);
-    getCommits(repo, displayCommits)
-}
-
-function displayCommits(commits){
-    console.log(commits)
+function displayCommits(commits) {
+  console.log(commits);
 }
 
 function getUser(id) {
-    return new Promise((resolve, reject)=>{
-        setTimeout(() => {
-          console.log("Reading a user from a db...");
-          resolve({ id: id, gitHubUserName: "zolamars" });
-        }, 2000);
-    })
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("Reading a user from a db...");
+      resolve({ id: id, gitHubUserName: "zolamars" });
+    }, 2000);
+  });
 }
 
 function getRepos(username) {
-    return new Promise((resolve, reject)=>{
-        setTimeout(() => {
-          console.log("username", username);
-          resolve(["repo1", "repo2", "repo3"]);
-        }, 2000);
-    })
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("username", username);
+      resolve(["repo1", "repo2", "repo3"]);
+    }, 2000);
+  });
 }
-function getCommits(repo){
-    return new Promise((resolve, reject  )=>{
-        setTimeout(()=>{
-            console.log('Calling GiHub API...')
-            resolve(['commit'])
-        },2000)
-    })
+function getCommits(repo) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("Calling GiHub API...");
+      resolve(["commit"]);
+    }, 2000);
+  });
 }
