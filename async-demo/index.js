c6fae358f983eeea5f1fd56b8ba06441142f80be
@zolabera <zolyemek@gmail.com>
2024-01-15 12:39:52 +0300

    using callbacks

--- async-demo/index.js
+++ async-demo/index.js
@@ -1,5 +1,21 @@
-console.log('Before')
-setTimeout(()=>{
-    console.log('Reading a user from a db')
-}, 2000)
-console.log('After')
+console.log("Before");
+getUser(1, (user) => {
+  getRepos(user.gitHubUserName, (userRepos) => {
+    console.log("User Repos", userRepos);
+  });
+});
+console.log("After");
+
+function getUser(id, callback) {
+  setTimeout(() => {
+    console.log("Reading a user from a db...");
+    callback({ id: id, gitHubUserName: "zolamars" });
+  }, 2000);
+}
+
+function getRepos(username, callback) {
+  setTimeout(() => {
+    console.log("username", username);
+    callback(["repo1", "repo2", "repo3"]);
+  }, 2000);
+}
