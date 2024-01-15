f8694b5b4b5d36b2e7136d82fe7d55236e3189f7
@zolabera <zolyemek@gmail.com>
2024-01-15 16:13:19 +0300

    An intro to promises

---
+++ async-demo/promise.js
@@ -0,0 +1,10 @@
+const p = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    // resolve(1) // pending => resolved, fulfilled
+    reject(new Error("Something went wrong.")); // pending => rejected
+  }, 2000);
+});
+
+p.then((result) => console.log("Result", result)).catch((err) =>
+  console.log("Error", err.message)
+);
