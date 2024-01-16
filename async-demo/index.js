2867717b02f67e85a6f3b1b00be67055fe16c3ac
@zolabera <zolyemek@gmail.com>
2024-01-16 16:21:53 +0300

    from promise-based approach to async and await

--- async-demo/index.js
+++ async-demo/index.js
@@ -1,16 +1,26 @@
 console.log("Before");
 
-getUser(1)
-  .then((user) => getRepos(user.gitHubUserName))
-  .then((repos) => getCommits(repos[0]))
-  .then((commits) => console.log("commits", commits))
-  .catch(err=> console.log('Error', err.message));
+// Promise-based approach
+// getUser(1)
+//   .then((user) => getRepos(user.gitHubUserName))
+//   .then((repos) => getCommits(repos[0]))
+//   .then((commits) => console.log("commits", commits))
+//   .catch(err=> console.log('Error', err.message));
 
-console.log("After");
-
-function displayCommits(commits) {
-  console.log(commits);
+// Async and Await
+async function displayCommits() {
+  try{
+    const user = await getUser(1);
+    const repos = await getRepos(user.gitHubUserName);
+    const commits = await getCommits(repos[0])
+    console.log(commits)
+  }catch(err){
+    console.log('Error', err.message)
+  }
 }
+
+displayCommits()
+console.log("After");
 
 function getUser(id) {
   return new Promise((resolve, reject) => {
@@ -26,6 +36,7 @@ function getRepos(username) {
     setTimeout(() => {
       console.log("username", username);
       resolve(["repo1", "repo2", "repo3"]);
+      // reject(new Error('Could not get the repos'))
     }, 2000);
   });
 }
